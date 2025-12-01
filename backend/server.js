d8e0867536e0ddde53bfa9521cc2fd995a9fbf6e
shivalika-jpg <shivalikaa.sinha2003@gmail.com>
2025-12-01 20:58:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,12 @@ require('dotenv').config();
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.json()); // for JSON bodies
-
-// routes
+app.use(express.json()); 
 const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/product');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
-
-// static uploads if you used local storage
 app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 5000;
